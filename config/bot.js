7c5dafda424ce0dbb2dc9060b00965c49a277ebf
Darshan Talwar <87927240+Slayer-Wolf@users.noreply.github.com>
2021-09-15 13:43:12 +0530

    Add files via upload
    Done

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODg3NTk2MjU4MDExNzgzMTY4.YUGcbg.NFhMo2sDLYQFjfUryLSPrO1kxlE',
+        prefix: '*',
+        activity: 'ACTIVITY',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
